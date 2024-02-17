253f0c7d70cf4b74d3360f64bb13f81ffcaef486
Daan Henke <daan@daan.vodka>
2024-02-17 07:48:32 -0800

    Added nuxt

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Test
+# Portfolio
+
+[aaa](https://go.c)
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0',
+    port: 1337
+  },
+  modules: [
+      '@nuxt/content',
+      '@nuxthq/studio'
+  ],
+  content: {
+
+  }
+})
